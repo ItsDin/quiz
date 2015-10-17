cf9a2114d9228f475b4a89b35546cefaab14a830
ItsDin <imdindini88@gmail.com>
2015-10-17 15:02:46 +0800

    Iterative Version and Add the boundary case
    Iterative version and add 2 boundary cases
    1: empty array
    2: eliminate non-english letters

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,31 @@
-/* FIXME: Implement! */
-
-int main()
-{
+/*Iterative Version and add the boundary case  */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#define SIZE 1000
+char smallest_character(char str[], char c){
+	int i;
+	//boundary case
+	if(str[0]==0){
+		assert(!"empty array");
+		return 0;
+	}else{
+		for(i=0; i<strlen(str); i++){
+			//English alphabet
+			if( !((str[i]>='A' && str[i]<='Z') || (str[i]>='a' && str[i]<='z'))){
+				return 0;
+			}else{
+				if(str[i] > c)
+					return str[i];
+			}
+		}
+	}
+	return str[0];
+}
+int main(){
+    char smallest;
+    smallest = smallest_character("cck", 'f');
+    printf("Smallest character : %c\n", smallest);
     return 0;
 }
