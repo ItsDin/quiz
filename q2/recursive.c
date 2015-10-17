2d4df84c5ee3b3b27614ec1b54024bf512c63cd8
ItsDin <imdindini88@gmail.com>
2015-10-17 15:47:07 +0800

    recursive version
    recursive version
    boundary case: 1.empty case 2.only English letters
    if first item is bigger than variable c, return it.
    If not, the pointer will point to next one.
    If you can't find the character larger than variable c,
    it will return the first one of array.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,36 @@
-/* FIXME: Implement! */
+/* FIXME: Recursive version and add boundary case */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#define SIZE 1000
+char smallest_character(char str[], char c){
+	char result;
+	if(str[0]==0){
+		assert(!"empty array");
+		return 0;
+	}else{
+		if( !((str[0]>='A' && str[0]<='Z') || (str[0]>='a' && str[0]<='z'))){
+			return 0;
+		}else{
+			if(str[0] > c){
+				return str[0];
+			}else{
+				result = smallest_character(str+1, c);
+				if(result > c){
+					return result;
+				}else{
+					return str[0];
+				}
+			}
+		}
+	}
+}
 
 int main()
 {
+    char smallest;
+    smallest = smallest_character("cck", 'f');
+    printf("Smallest character : %c\n", smallest);
     return 0;
 }
